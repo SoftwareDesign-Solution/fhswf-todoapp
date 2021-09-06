f93640f01ff1d7a4501d6d2af93d71836b168786
softwaredesign-solution <mail@softwaredesign-solution.de>
2021-09-06 13:18:30 +0200

    - ESLint-Regelkatalog angewendet

--- src/services/Api.Service.ts
+++ src/services/Api.Service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any */
 import axios, {AxiosInstance} from "axios";
 import {singleton} from "tsyringe";
 
